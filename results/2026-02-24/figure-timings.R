971139f9d451e73771b7738e4e4ca96169a03fe8
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-02-26 00:21:51 -0500

    laptop timings

--- results/2026-02-24/figure-timings.R
+++ results/2026-02-24/figure-timings.R
@@ -1,15 +1,19 @@
 library(data.table)
 library(ggplot2)
 seq_dt <- fread("NSCH_local.csv")
+laptop_dt <- fread("NSCH_local_laptop.csv")
 proj_dt <- fread("NSCH_mpi.csv")
 mpi_dt <- proj_dt[, .(
   cpus=length(unique(process))+1, host="rorqual", pkg="mlr3resampling",
   started=start.time, done=end.time, learner_id, job.id=process, row=.I)]
 reg_dt <- fread("NSCH_batchtools_jobs.csv")
 sec_all_dt <- rbind(
+  laptop_dt[, .(
+    cpus=length(unique(process)), host="laptop", pkg="mlr3resampling",
+    started=start.time, done=end.time, learner_id, job.id=as.integer(factor(process)), row=.I)],
   seq_dt[, .(
-    cpus=1, host="laptop", pkg="mlr3resampling",
-    started=start.time, done=end.time, learner_id, job.id=1, row=.I)],
+    cpus=6, host="desktop", pkg="mlr3resampling",
+    started=start.time, done=end.time, learner_id, job.id=.I, row=.I)],
   reg_dt[, .(
     cpus=.N, host="rorqual", pkg="mlr3batchmark",
     started, done, learner_id, job.id, row=.I)],
@@ -34,7 +38,9 @@ all_dt <- dcast(
 blank_dt <- data.table(mpi_dt[1], x=0, y=21)
 gg <- ggplot()+
   facet_grid(cpus+pkg+host~., labeller=label_both, scales="free")+
-  scale_y_continuous(breaks=c(1,seq(10, 100, by=10)))+
+  scale_y_continuous(
+    "process",
+    breaks=c(1,seq(10, 100, by=10)))+
   scale_x_continuous(
     "Minutes from start of computation (train + test cv_glmnet learner on NSCH_data)")+
   geom_segment(aes(
@@ -49,6 +55,6 @@ gg <- ggplot()+
   geom_blank(aes(x,y),data=blank_dt)
 png(
   "figure-timings.png",
-  width=8, height=5, units="in", res=200)
+  width=8, height=6, units="in", res=200)
 print(gg)
 dev.off()
